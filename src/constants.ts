21e4a04074f0ad16731fcf2d7744043f5dbe4257
DevHyperCoder <arduinoleo88@gmail.com>
2021-01-14 10:23:38 +0530

    Refactor commands to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,14 @@
+import { homedir } from "os";
+import { join } from "path";
+
 export const EXTENSION_ID = "flutter-matic";
-export const INSTALL_FLUTTER_COMMAND = "install-flutter";
+export const INSTALL_FLUTTER_COMMAND = "install-flutter"
+
+export const gitCloneCommand = 
+      `git clone --depth 1 --branch beta https://github.com/flutter/flutter.git "${join(
+        homedir(),
+        ".flutter-sdktest"
+      )}"`;
+
+export const flutterCommand = `flutter`;
+export const configEnableWebFlutter = `flutter config --enable-web`;
--- src/installer/installFlutter.ts
+++ src/installer/installFlutter.ts
@@ -1,16 +1,11 @@
-import { homedir } from "os";
-import { join } from "path";
 import { Output } from "../Output";
 import { exec } from "../runCommand";
 
+import {gitCloneCommand,flutterCommand,configEnableWebFlutter} from "../constants"
+
 export async function gitClone(): Promise<Output> {
   try {
-    await exec(
-      `git clone --depth 1 --branch beta https://github.com/flutter/flutter.git "${join(
-        homedir(),
-        ".flutter-sdktest"
-      )}"`
-    );
+    await exec(gitCloneCommand);
     return { success: true, info: "Successfully cloned git repo" };
   } catch (e: any) {
     return {
@@ -22,7 +17,7 @@ export async function gitClone(): Promise<Output> {
 
 export async function installFlutter(): Promise<Output> {
   try {
-    await exec("flutter");
+    await exec(flutterCommand);
     return { success: true, info: "Flutter installed successfully" };
   } catch (e: any) {
     return { success: false, error: e.message };
@@ -31,10 +26,7 @@ export async function installFlutter(): Promise<Output> {
 
 export async function configureFlutter(): Promise<Output> {
   try {
-
-      await exec(
-        `flutter config --enable-web`
-      );
+      await exec(configEnableWebFlutter);
     return { success: true, info: "Flutter configuration complete!" };
   }
   catch (e: any) {
@@ -48,10 +40,9 @@ export async function createFlutterWebApp(
 ): Promise<Output> {
   try {
     await exec(
-      `${join(homedir(), ".flutter-sdktest", "bin", "flutter")} create ${name}`,
+      `flutter create ${name}`,
       { cwd: path }
     );
-
     return { success: true, info: "Flutter WebApp created\n" };
   } catch (e: any) {
     return { success: false, error: e.message };
